5e38820da3abe82ea93ce906a5c6a9c63b88b718
Bryce Groff <bgroff@hawaii.edu>
2023-01-19 19:52:57 -0800

    Update the WellKnown endpoints to use the AuthorizationServers URL.

--- src/main/java/com/cartobucket/auth/routes/WellKnown.java
+++ src/main/java/com/cartobucket/auth/routes/WellKnown.java
@@ -2,28 +2,42 @@ package com.cartobucket.auth.routes;
 
 import com.cartobucket.auth.generated.WellKnownApi;
 import com.cartobucket.auth.model.generated.AccessTokenRequest;
+import com.cartobucket.auth.services.AuthorizationServerService;
 import jakarta.ws.rs.core.Response;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 public class WellKnown implements WellKnownApi {
+    final AuthorizationServerService authorizationServerService;
+
+    public WellKnown(AuthorizationServerService authorizationServerService) {
+        this.authorizationServerService = authorizationServerService;
+    }
+
     @Override
     public Response authorizationServerIdWellKnownOpenidConnectGet(UUID authorizationServerId) {
+        final var authorizationServer = authorizationServerService.getAuthorizationServer(authorizationServerId);
         var wellKnown = new com.cartobucket.auth.model.generated.WellKnown();
-        wellKnown.setIssuer("https://sso.cartobucket.com/");
-        wellKnown.setAuthorizationEndpoint("https://sso.cartobucket.com/authorizationServer/authorization/");
-        wellKnown.setTokenEndpoint("https://sso.cartobucket.com/authorizationServer/token/");
-        wellKnown.setJwksUri("https://sso.cartobucket.com/authorizationServer/jwks/");
-        wellKnown.setRevocationEndpoint("https://sso.cartobucket.com/authorizationServer/revocation/");
-        wellKnown.setUserinfoEndpoint("https://sso.cartobucket.com/authorizationServer/userinfo/");
+        wellKnown.setIssuer(String.valueOf(authorizationServer.getServerUrl()));
+        wellKnown.setAuthorizationEndpoint(
+                authorizationServer.getServerUrl() + "/" + authorizationServer.getId() + "/authorization/");
+        wellKnown.setTokenEndpoint(
+                authorizationServer.getServerUrl() + "/" + authorizationServer.getId() + "token/");
+        wellKnown.setJwksUri(
+                authorizationServer.getServerUrl() + "/" + authorizationServer.getId() + "/jwks/");
+        wellKnown.setRevocationEndpoint(
+                authorizationServer.getServerUrl() + "/" + authorizationServer.getId() + "/revocation/");
+        wellKnown.setUserinfoEndpoint(
+                authorizationServer.getServerUrl() + "/" + authorizationServer.getId() + "/userinfo/");
         wellKnown.setTokenEndpointAuthMethodsSupported(
-                Arrays.asList(
+                List.of(
                         com.cartobucket.auth.model.generated.WellKnown.TokenEndpointAuthMethodsSupportedEnum.POST
                 )
         );
         wellKnown.setIdTokenSigningAlgValuesSupported(
-                Arrays.asList(
+                List.of(
                         com.cartobucket.auth.model.generated.WellKnown.IdTokenSigningAlgValuesSupportedEnum.RS256
                 )
         );
@@ -34,7 +48,7 @@ public class WellKnown implements WellKnownApi {
                         com.cartobucket.auth.model.generated.WellKnown.ResponseTypesSupportedEnum.TOKEN
                 )
         );
-        wellKnown.setCodeChallengeMethodsSupported(Arrays.asList(com.cartobucket.auth.model.generated.WellKnown.CodeChallengeMethodsSupportedEnum.S256));
+        wellKnown.setCodeChallengeMethodsSupported(List.of(com.cartobucket.auth.model.generated.WellKnown.CodeChallengeMethodsSupportedEnum.S256));
         wellKnown.setGrantTypesSupported(
                 Arrays.asList(
                         AccessTokenRequest.GrantTypeEnum.CLIENT_CREDENTIALS.value(),
