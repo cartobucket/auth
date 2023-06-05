4ad3090ceb4d2a191466c024761cfa4808a13748
Bryce Groff <bgroff@hawaii.edu>
2023-06-05 15:54:59 -0700

    Add the proto server and copy a bunch of stuff around. This is a mess right now but I am travelling and need to take this with me.

---
+++ auth-data-server/src/main/java/com/cartobucket/auth/models/AccessToken.java
@@ -0,0 +1,233 @@
+
+package com.cartobucket.auth.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Objects;
+
+/**
+* 
+**/
+
+@JsonTypeName("AccessTokenResponse")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2023-05-29T17:50:42.716026387-07:00[America/Los_Angeles]")
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+
+public class AccessToken {
+    private  String accessToken;
+            public enum TokenTypeEnum {
+
+    BEARER(String.valueOf("Bearer"));
+
+
+    private String value;
+
+    TokenTypeEnum (String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    /**
+     * Convert a String into String, as specified in the
+     * <a href="https://download.oracle.com/otndocs/jcp/jaxrs-2_0-fr-eval-spec/index.html">See JAX RS 2.0 Specification, section 3.2, p. 12</a>
+     */
+	public static TokenTypeEnum fromString(String s) {
+        for (TokenTypeEnum b : TokenTypeEnum.values()) {
+            // using Objects.toString() to be safe if value type non-object type
+            // because types like 'int' etc. will be auto-boxed
+            if (Objects.toString(b.value).equals(s)) {
+                return b;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected string value '" + s + "'");
+	}
+	
+    @JsonCreator
+    public static TokenTypeEnum fromValue(String value) {
+        for (TokenTypeEnum b : TokenTypeEnum.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected value '" + value + "'");
+    }
+}
+
+    private  TokenTypeEnum tokenType;
+    private  String refreshToken;
+    private  Integer expiresIn;
+    private  String idToken;
+    private  String scope;
+
+    /**
+    **/
+    public AccessToken accessToken(String accessToken) {
+    this.accessToken = accessToken;
+    return this;
+    }
+
+    
+    @JsonProperty("access_token")
+      public String getAccessToken() {
+    return accessToken;
+    }
+
+    @JsonProperty("access_token")
+    public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+    }
+
+    /**
+    **/
+    public AccessToken tokenType(TokenTypeEnum tokenType) {
+    this.tokenType = tokenType;
+    return this;
+    }
+
+    
+    @JsonProperty("token_type")
+      public TokenTypeEnum getTokenType() {
+    return tokenType;
+    }
+
+    @JsonProperty("token_type")
+    public void setTokenType(TokenTypeEnum tokenType) {
+    this.tokenType = tokenType;
+    }
+
+    /**
+    **/
+    public AccessToken refreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+    return this;
+    }
+
+    
+    @JsonProperty("refresh_token")
+      public String getRefreshToken() {
+    return refreshToken;
+    }
+
+    @JsonProperty("refresh_token")
+    public void setRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+    }
+
+    /**
+    **/
+    public AccessToken expiresIn(Integer expiresIn) {
+    this.expiresIn = expiresIn;
+    return this;
+    }
+
+    
+    @JsonProperty("expires_in")
+      public Integer getExpiresIn() {
+    return expiresIn;
+    }
+
+    @JsonProperty("expires_in")
+    public void setExpiresIn(Integer expiresIn) {
+    this.expiresIn = expiresIn;
+    }
+
+    /**
+    **/
+    public AccessToken idToken(String idToken) {
+    this.idToken = idToken;
+    return this;
+    }
+
+    
+    @JsonProperty("id_token")
+      public String getIdToken() {
+    return idToken;
+    }
+
+    @JsonProperty("id_token")
+    public void setIdToken(String idToken) {
+    this.idToken = idToken;
+    }
+
+    /**
+        * 
+    **/
+    public AccessToken scope(String scope) {
+    this.scope = scope;
+    return this;
+    }
+
+    
+    @JsonProperty("scope")
+      public String getScope() {
+    return scope;
+    }
+
+    @JsonProperty("scope")
+    public void setScope(String scope) {
+    this.scope = scope;
+    }
+
+
+@Override
+public boolean equals(Object o) {
+if (this == o) {
+return true;
+}
+if (o == null || getClass() != o.getClass()) {
+return false;
+}
+    AccessToken accessTokenResponse = (AccessToken) o;
+    return Objects.equals(this.accessToken, accessTokenResponse.accessToken) &&
+    Objects.equals(this.tokenType, accessTokenResponse.tokenType) &&
+    Objects.equals(this.refreshToken, accessTokenResponse.refreshToken) &&
+    Objects.equals(this.expiresIn, accessTokenResponse.expiresIn) &&
+    Objects.equals(this.idToken, accessTokenResponse.idToken) &&
+    Objects.equals(this.scope, accessTokenResponse.scope);
+}
+
+@Override
+public int hashCode() {
+return Objects.hash(accessToken, tokenType, refreshToken, expiresIn, idToken, scope);
+}
+
+@Override
+public String toString() {
+StringBuilder sb = new StringBuilder();
+sb.append("class AccessTokenResponse {\n");
+
+sb.append("    accessToken: ").append(toIndentedString(accessToken)).append("\n");
+sb.append("    tokenType: ").append(toIndentedString(tokenType)).append("\n");
+sb.append("    refreshToken: ").append(toIndentedString(refreshToken)).append("\n");
+sb.append("    expiresIn: ").append(toIndentedString(expiresIn)).append("\n");
+sb.append("    idToken: ").append(toIndentedString(idToken)).append("\n");
+sb.append("    scope: ").append(toIndentedString(scope)).append("\n");
+sb.append("}");
+return sb.toString();
+}
+
+/**
+* Convert the given object to string with each line indented by 4 spaces
+* (except the first line).
+*/
+private String toIndentedString(Object o) {
+if (o == null) {
+return "null";
+}
+return o.toString().replace("\n", "\n    ");
+}
+
+
+}
---
+++ auth-data-server/src/main/java/com/cartobucket/auth/models/ClientCode.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2023 Bryce Groff (Cartobucket LLC)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cartobucket.auth.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class ClientCode {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private UUID clientId;
+
+    private UUID authorizationServerId;
+
+    private OffsetDateTime createdOn;
+
+    private String redirectUri;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private List<String> scopes;
+
+    private String state;
+
+    private String code;
+
+    private String codeChallenge;
+
+    private String codeChallengeMethod;
+
+    private UUID userId;
+
+    private String nonce;
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public UUID getAuthorizationServerId() {
+        return authorizationServerId;
+    }
+
+    public void setAuthorizationServerId(UUID authorizationServerId) {
+        this.authorizationServerId = authorizationServerId;
+    }
+
+    public OffsetDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(OffsetDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+
+    public List<String> getScopes() {
+        return scopes;
+    }
+
+    public void setScopes(List<String> scope) {
+        this.scopes = scope;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCodeChallenge() {
+        return codeChallenge;
+    }
+
+    public void setCodeChallenge(String codeChallenge) {
+        this.codeChallenge = codeChallenge;
+    }
+
+    public String getCodeChallengeMethod() {
+        return codeChallengeMethod;
+    }
+
+    public void setCodeChallengeMethod(String codeChallengeMethod) {
+        this.codeChallengeMethod = codeChallengeMethod;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
+}
